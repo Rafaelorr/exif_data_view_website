96abd579ac8f1c2421164bca397cf66599a004dd
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-08-01 20:38:03 +0200

    add docs route to app.py

--- app.py
+++ app.py
@@ -37,5 +37,9 @@ def upload():
 
     return render_template("result.html", exif=exif_data)
 
+@app.route("/docs")
+def documentation():
+    return render_template("exif_tags.html")
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
