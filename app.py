7ded9dd59a6c60bc45fc0cefa16404ae7cadbf22
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-08-01 16:12:30 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template, send_file
+from PIL import Image
+import os
+from io import BytesIO
+from functions import *
+import tempfile
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files.get('image')
+    if not file:
+        return "No file uploaded", 400
+
+    # Save file to a temporary location
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
+        file.save(tmp.name)
+        tmp_path = tmp.name
+
+    # Read EXIF data
+    exif_data = read_exif_data(tmp_path)
+
+    # make GPS data human readable
+    lat, lon = extract_gps_location(exif_data["GPSInfo"])
+    exif_data["GPSInfo"] = {"lat":lat, "lon":lon}
+
+    # Clean up the temporary file
+    os.remove(tmp_path)
+
+    # Render the EXIF data (adjust resulat.html accordingly)
+    return render_template("resulat.html", exif=exif_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,46 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+def dms_to_decimal(degrees, minutes, seconds, direction):
+    """Convert degrees, minutes, seconds to decimal format."""
+    decimal = degrees + minutes / 60.0 + seconds / 3600.0
+    if direction in ['S', 'W']:
+        decimal = -decimal
+    return decimal
+
+def extract_gps_location(gps_info):
+    """Extract and convert GPS coordinates to decimal degrees."""
+    lat_ref = gps_info.get(1)
+    lat = gps_info.get(2)
+    lon_ref = gps_info.get(3)
+    lon = gps_info.get(4)
+
+    if lat and lon and lat_ref and lon_ref:
+        lat_decimal = dms_to_decimal(*lat, lat_ref)
+        lon_decimal = dms_to_decimal(*lon, lon_ref)
+        return lat_decimal, lon_decimal
+    else:
+        return None, None
+
+def read_exif_data(image_path):
+    exclusions :tuple = ("MakerNote","UserComment","ComponentsConfiguration","ExifVersion","FlashPixVersion","FileSource","SceneType")
+    try:
+        image = Image.open(image_path)
+        exif_data = image._getexif()
+
+        if not exif_data:
+            return None
+
+        image_exif_data :dict = {}
+        for tag_id, value in exif_data.items():
+            tag = TAGS.get(tag_id, tag_id)
+            if tag in exclusions:
+                continue
+            image_exif_data[tag] = value
+        
+        return image_exif_data
+
+    except FileNotFoundError:
+        print(f"File not found: {image_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
